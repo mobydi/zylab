acb5652be1a8beb75d6e442b8ddaab6ec7818ad4
Ilya <mobydi@gmail.com>
2016-02-13 13:48:02 +0300

    start

--- Zylab.Interview.BinStorage/BinaryStorage.cs
+++ Zylab.Interview.BinStorage/BinaryStorage.cs
@@ -3,16 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.IO;
+using System.IO.MemoryMappedFiles;
 
 namespace Zylab.Interview.BinStorage {
     public class BinaryStorage : IBinaryStorage {
 
+		Dictionary<string, Int64> index = new Dictionary<string, Int64> ();
+
         public BinaryStorage(StorageConfiguration configuration) {
 
+			string storageFile = Path.Combine (configuration.WorkingFolder, "storage.bin");
+			if (!File.Exists (storageFile))
+				File.Create (storageFile).Close ();
         }
 
         public void Add(string key, Stream data) {
-            throw new NotImplementedException();
+			if (index.ContainsKey (key))
+				throw new Exception ();
+
+			//increase file size
+
         }
 
         public Stream Get(string key) {
