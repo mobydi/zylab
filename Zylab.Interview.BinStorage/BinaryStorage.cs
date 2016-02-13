dc7a6854ca5ee347526b77a04ff2d9c0b5e2c8bb
Ilya <mobydi@gmail.com>
2016-02-13 14:03:21 +0300

    lock

--- Zylab.Interview.BinStorage/BinaryStorage.cs
+++ Zylab.Interview.BinStorage/BinaryStorage.cs
@@ -9,20 +9,27 @@ namespace Zylab.Interview.BinStorage {
     public class BinaryStorage : IBinaryStorage {
 
 		Dictionary<string, Int64> index = new Dictionary<string, Int64> ();
+		readonly FileStream storageSream;
+		readonly object writeLock = new object();
 
         public BinaryStorage(StorageConfiguration configuration) {
 
-			string storageFile = Path.Combine (configuration.WorkingFolder, "storage.bin");
+			storageFile = Path.Combine (configuration.WorkingFolder, "storage.bin");
 			if (!File.Exists (storageFile))
 				File.Create (storageFile).Close ();
+			storageSream = new FileStream(storageFile, FileMode.Open, FileAccess.ReadWrite, FileShare.None,
+				        bufferSize: 4096, useAsync: true);
         }
 
         public void Add(string key, Stream data) {
 			if (index.ContainsKey (key))
 				throw new Exception ();
-
-			//increase file size
-
+		
+			Int64 positionToWrite;
+			lock (writeLock) {
+				positionToWrite = storageSream.Length;
+				storageSream.Seek (data.Length, SeekOrigin.End);
+			}
         }
 
         public Stream Get(string key) {
